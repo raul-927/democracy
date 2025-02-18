be0dd7c1aac2fa5697a74411d8c85f1857b378de
raul <raraherher9274@gmail.com>
2025-02-18 18:05:18 -0500

    Se agrega Penal y se refactoriza codigo

---
+++ Microservices/StateMachine/src/main/java/com/democracy/infrastructure/events/EventEnum.java
@@ -0,0 +1,5 @@
+package com.democracy.infrastructure.events;
+
+public enum EventEnum {
+    CALL_INVOICES, CALL_END;
+}
---
+++ Microservices/StateMachine/src/main/java/com/democracy/infrastructure/satates/StateEnum.java
@@ -0,0 +1,5 @@
+package com.democracy.infrastructure.satates;
+
+public enum StateEnum {
+    INDEX, CALCULATE_INVOICES, END;
+}
