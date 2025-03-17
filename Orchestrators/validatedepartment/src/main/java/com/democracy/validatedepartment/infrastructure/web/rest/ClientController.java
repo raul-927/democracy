8d2ab895b5b97f702931af730e46a73744afd791
raul <raraherher9274@gmail.com>
2025-03-17 17:48:19 -0500

    Start Orchestrators

--- Orchestrators/validatedepartment/src/main/java/com/democracy/validatedepartment/infrastructure/web/rest/ClientController.java
+++ Orchestrators/validatedepartment/src/main/java/com/democracy/validatedepartment/infrastructure/web/rest/ClientController.java
@@ -6,6 +6,7 @@ import com.democracy.validatedepartment.application.services.OrderService;
 import com.democracy.validatedepartment.domain.models.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -19,15 +20,17 @@ public class ClientController {
     @Autowired
     private OrderService orderService;
 
-    @RequestMapping("/goal")
-    public String hello() {
+    @PostMapping("/goal")
+    public String hello(@RequestHeader(value="Authorization") String userAgent) {
+        System.out.println("AUTHORIZATION: "+userAgent);
         Order returnOrder = orderService.newOrder(new Order());
         return "OK";
     }
 
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.OK)
-    public Mono<Order> newOrder(@RequestBody Order order){
+    public Mono<Order> newOrder(@RequestBody Order order, @RequestHeader(value="Authorization") String userAgent){
+        System.out.println("AUTHORIZATION: "+userAgent);
         return Mono.just(orderService.newOrder(order));
     }
 
