71e7e430cf927cf58c3f103cb939a64ad8c28126
raul <raraherher9274@gmail.com>
2025-03-19 09:28:36 -0500

    Se refactoriza StateMachineApplication

--- Microservices/HumanResources/src/main/java/com/democracy/hhrr/infrastructure/constants/RouterConstant.java
+++ Microservices/HumanResources/src/main/java/com/democracy/hhrr/infrastructure/constants/RouterConstant.java
@@ -1,14 +1,17 @@
 package com.democracy.hhrr.infrastructure.constants;
 
 public class RouterConstant {
+    //--------------------GENERIC PATH---------------------------------
     public static final String PATH_MAIN = "/humanresources";
-    public static final String DEPARTMENT_PATH = "/department";
-    public static final String STREET_PATH = "/street";
     public static final String PATH_SELECT_ALL ="/select-all";
     public static final String PATH_SELECT = "/select";
     public static final String PATH_SAVE = "/save";
     public static final String PATH_INSERT = "/insert";
     public static final String PATH_UPDATE = "/update";
     public static final String PATH_COUNT="/select-count";
+    //--------------------DOMAIN PATH---------------------------------------
+    public static final String DEPARTMENT_PATH = "/department";
+    public static final String STREET_PATH = "/street";
+
 
 }
