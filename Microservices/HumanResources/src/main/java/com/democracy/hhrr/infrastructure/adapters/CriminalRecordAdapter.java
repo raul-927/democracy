71df5ae5397976f22a4f34010f62e8a831bf5284
raul <raraherher9274@gmail.com>
2025-03-28 14:50:32 -0500

    Se agregan servicios en Human Resources

--- Microservices/HumanResources/src/main/java/com/democracy/hhrr/infrastructure/adapters/CriminalRecordAdapter.java
+++ Microservices/HumanResources/src/main/java/com/democracy/hhrr/infrastructure/adapters/CriminalRecordAdapter.java
@@ -2,39 +2,52 @@ package com.democracy.hhrr.infrastructure.adapters;
 
 import com.democracy.hhrr.domain.models.CriminalRecord;
 import com.democracy.hhrr.domain.ports.out.CriminalRecordOut;
+import com.democracy.hhrr.infrastructure.repository.mybatis.r2dbc.mappers.CriminalRecordMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 public class CriminalRecordAdapter implements CriminalRecordOut {
+
+    @Autowired
+    private CriminalRecordMapper criminalRecordMapper;
+
+
     @Override
     public Mono<Integer> createCriminalRecord(CriminalRecord criminalRecord) {
-        return null;
+
+        return criminalRecordMapper.insert(criminalRecord);
     }
 
     @Override
     public Mono<Integer> deleteCriminalRecord(String criminalRecordId) {
-        return null;
+
+        return criminalRecordMapper.deleteCriminalRecord(criminalRecordId);
     }
 
     @Override
     public Flux<CriminalRecord> selectCriminalRecord(CriminalRecord criminalRecord) {
-        return null;
+
+        return criminalRecordMapper.selectFullCriminalRecord(criminalRecord);
     }
 
     @Override
     public Flux<CriminalRecord> selectAllDCriminalRecords() {
+
         return null;
     }
 
     @Override
     public Mono<Long> selectCount() {
-        return null;
+
+        return criminalRecordMapper.count();
     }
 
     @Override
     public Mono<Integer> updateCriminalRecord(CriminalRecord criminalRecord) {
-        return null;
+
+        return criminalRecordMapper.updateSelectiveByPrimaryKey(criminalRecord);
     }
 }
