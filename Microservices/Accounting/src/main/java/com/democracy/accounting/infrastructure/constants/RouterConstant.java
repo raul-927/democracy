276482b6610d9d3da610f70df826b00a8b52b522
Raul <raraherher9274@gmail.com>
2026-07-03 12:39:37 -0500

    Se corrije update PersonDynamicMapper.java

---
+++ Microservices/Accounting/src/main/java/com/democracy/accounting/infrastructure/constants/RouterConstant.java
@@ -0,0 +1,26 @@
+package com.democracy.accounting.infrastructure.constants;
+
+public class RouterConstant {
+    //--------------------GENERIC PATH---------------------------------
+    public static final String PATH_MAIN = "/electoralcourt";
+    public static final String PATH_SELECT_ALL ="/select-all";
+    public static final String PATH_SELECT = "/select";
+    public static final String PATH_SAVE = "/save";
+    public static final String PATH_INSERT = "/insert";
+    public static final String PATH_UPDATE = "/update";
+    public static final String PATH_COUNT="/select-count";
+    public static final String DELETE ="/delete";
+    //--------------------DOMAIN PATH---------------------------------------
+    public static final String INVESTIGATION_PATH = "/investigation";
+    public static final String STREET_PATH = "/street";
+    public static final String INVESTIGATION ="/investigation";
+    public static final String  PENAL = "/penal";
+    public static final String PROFESSION = "/profession";
+    public static final String PERSON ="/person";
+    public static final String CRIMINAL_RECORD ="/criminalrecord";
+    public static final String QUALIFICATION = "/qualification";
+    public static final String DOCUMENT = "/document";
+    public static final String INSTITUTE = "/institute";
+
+
+}
